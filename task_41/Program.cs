2c99dad3c81d1c6c98a5443585b092070712395e
Prozherin <taskacik@gmail.com>
2023-09-09 15:27:06 +0500

    домашняя работа №6

---
+++ task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа через пробел:");
+        string numbers = Console.ReadLine();
+
+        string[] numbersArray = numbers.Split(' ');
+        int count = 0;
+
+        foreach (string num in numbersArray)
+        {
+            if (int.Parse(num) > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел больше 0: " + count);
+    }
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double b1, k1, b2, k2;
+        Console.Write("Введите значение b1: ");
+        b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение k1: ");
+        k1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение b2: ");
+        b2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите значение k2: ");
+        k2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine("Точка пересечения: ({0}; {1})", x, y);
+    }
+}
